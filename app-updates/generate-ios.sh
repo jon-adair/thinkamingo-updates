0476ea070df05280c31af40b79f79ed9ccdff1b2
Jon Adair <jonadair@gmail.com>
2015-11-30 21:26:48 -0500

    quickie way to generate the html

---
+++ app-updates/generate-ios.sh
@@ -0,0 +1,12 @@
+cp more-l4w/index.html ./i
+
+sed 's/more-l4w/more-nd/' < ./i > more-name-dice/index.html
+sed 's/more-l4w/more-sd/' < ./i > more-sd/index.html
+sed 's/more-l4w/more-ss/' < ./i > more-ss/index.html 
+sed 's/more-l4w/more-sks/' < ./i > more-sks/index.html 
+sed 's/more-l4w/more-st/' < ./i > more-spy-tools/index.html 
+sed 's/more-l4w/more-ssd/' < ./i > more-spooky/index.html 
+sed 's/more-l4w/more-qiiq/' < ./i > more-qiiq/index.html
+#sed 's/more-l4w/more-scp/' < ./i > more-sports-cards/index.html 
+
+rm i
